87e5ca85ec912217548214fe2c782609ac01d33f
Jose Acuña <jodaniel1409@gmail.com>
2020-04-10 22:50:15 -0600

    Added Side Navbar

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -10,12 +10,6 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
   constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
